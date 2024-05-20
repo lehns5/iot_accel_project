69deb10aac20c0bed284ebf11e8b1f02189fa3dc
lehns5 <invaderinder@gmail.com>
2024-05-20 09:19:08 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,22 +1,31 @@
 # SETUP WIFI CONECTION
 import network
 from time import sleep
+from WIFI_CONFIG import networks
 
-sta_if = network.WLAN(network.STA_IF)
-networks=[['domih','DomisPW1'],['Tripple L', '15263748']]
-SSID = 'domih'
-PW = 'DomisPW1'
+connected_network = None
 
-if not sta_if.isconnected():
-  print('connecting to network...')
-  for i in range(len(networks)):
-    try:
-      sta_if.active(True)
-      sta_if.connect(networks[i][0], networks[i][1])
-    except:
-      pass
+def connect_to_wifi():
+    global connected_network
+    sta_if = network.WLAN(network.STA_IF)
+    
+    if not sta_if.isconnected():
+        print('Connecting to network...')
+        for ssid, password in networks:
+            sta_if.active(True)
+            sta_if.connect(ssid, password)
+            for _ in range(10):
+                if sta_if.isconnected():
+                    break
+                sleep(1)
+            if sta_if.isconnected():
+                print(f'Connected to {ssid}')
+                connected_network = (ssid, password)
+                break
+        else:
+            print('Failed to connect to any network')
+    
+    if sta_if.isconnected():
+        print('Network config:', sta_if.ifconfig())
 
-  while not sta_if.isconnected():
-    pass
-
-print('network config:', sta_if.ifconfig())
+connect_to_wifi()
